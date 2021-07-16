e66447cf6967516d381ae90f5887dab34d799538
Legolazzs <87474915+Legolazzs@users.noreply.github.com>
2021-07-16 12:05:13 +0200

    Update main_install.sh

--- main_install.sh
+++ main_install.sh
@@ -19,4 +19,4 @@ git clone https://github.com/nextcloud/docker.git nextcloud
 cd nextcloud/.examples/docker-compose/insecure/postgres/apache
 
 echo -e "${bg_purple}  ----------> start nextcloud <----------  ${reset}"
-sudo docker-compose up 
+docker-compose up 
